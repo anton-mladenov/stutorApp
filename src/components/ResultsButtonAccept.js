c49f529489dbe68d3439ad558aef2ab2baef8d12
rbhgn <robhgn@gmail.com>
2018-06-19 12:11:27 +0200

    made first button

---
+++ src/components/ResultsButtonAccept.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function resultsButtonAccepts (props) {
+  return (
+    <input
+      type="submit"
+      class="button-accepts"
+      value=">"
+    />
+
+  )
+}
--- src/components/resultsContainer.js
+++ src/components/resultsContainer.js
@@ -1,11 +1,12 @@
-import React, {PureComponent} from 'react'
-
+import React, { PureComponent } from 'react'
+import ResultsButtonAccepts from './ResultsButtonAccepts'
 
 export default class resultsContainer extends PureComponent {
   render () {
-      return (
-        <div className="results-container">RESULTS CONTAINER
-        </div>
-  )
+    return (
+      <div className="results-container">
+        <ResultsButtonAccepts userId="1" />
+      </div>
+    )
   }
 }
