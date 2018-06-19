20d821a3279841a761a1aabe3f56a60e5154e465
rbhgn <robhgn@gmail.com>
2018-06-19 21:34:32 +0200

    Added picture and made responsive design resultspage

--- src/components/ResultsUserContainer.js
+++ src/components/ResultsUserContainer.js
@@ -2,24 +2,28 @@ import React from 'react'
 import './Results.css';
 import ResultsButtonAccept from './ResultsButtonAccept'
 import ResultsButtonReject from './ResultsButtonReject'
+import testProfile from '../usersData/test-profile.jpg'
 
 export default function ResultsUserContainer (props) {
   return (
     <div className="results-user-container">
+      <div className="results-user-container-type">
+        <p className="results-user-container-type-text">TUTOR</p>
+      </div>
       <div className="results-user-container-skill">
-        <p>Skill</p>
+        <p className="results-user-container-skill-text">Javascript</p>
       </div>
       <div className="results-user-container-photo">
-        <p>Photo</p>
+        <img className="results-user-container-photo-img" src={ testProfile } alt="name"/>
       </div>
       <div className="results-user-container-name">
-        <p>Firstname</p>
+        <p className="results-user-container-name-text">Firstname</p>
       </div>
       <div className="results-user-container-location">
-        <p>City, Distance</p>
+        <p className="results-user-container-location-text">City, Distance</p>
       </div>
       <div className="results-user-container-bio">
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
+        <p className="results-user-container-bio-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
       </div>
       <div className="results-buttons-container">
         <ResultsButtonReject />
