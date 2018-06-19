274df724815ecd30f5ff94406834db8652174a11
anton-mladenov <tonkata@mail.com>
2018-06-19 13:58:00 +0200

    and another commit

--- src/usersData/usersDummyData.js
+++ src/usersData/usersDummyData.js
@@ -1,64 +1,108 @@
 let tutorsSkillSet = [
-	"HTML/CSS",
-	"Design",
-	"React",
-	"NodeJS",
-	"Marketing"
+	{id: 1,
+	name: "HTML/CSS"},
+	{id: 2,
+	name: "Design"},
+	{id: 3,
+	name: "React"},
+	{id: 4,
+	name: "NodeJS"},
+	{id: 5,
+	name: "Marketing"}
 ]
 
 let students = [
 	{
-		id: "S01"
-		name: "Stan",
-		shortBio: "likes Eminem",
-		likesTutorID: ["T01"]
+		id: 1,
+		firstName: "Hilargi",
+		lastName: "Alecto",
+		email: "hilargi@gmail.com",
+		shortBio: "I am a 47-year-old associate at a law firm who enjoys listening to music, jigsaw puzzles and baking.",		
+		rejected: [4],
+		accepted: [7,9]
 	},
 	{
-		id: "S02"
-		name: "Danny",
-		shortBio: "hates people",
-		likesTutorID: ["T01", "T02"]
+		id: 2,
+		firstName: "Keara",
+		lastName: "Naseem",
+		email: "keara@gmail.com",
+		shortBio: "I am a 61-year-old CEO who enjoys donating blood, jigsaw puzzles and adult colouring books.",		
+		rejected: [7, 4],
+		accepted: [9]
 	},
 	{
-		id: "S03"
-		name: "Cleopatra",
-		shortBio: "take baths in milk",
-		likesTutorID: ["T03"]
+		id: 3,
+		firstName: "Svetlana",
+		lastName: "Vulcan",
+		email: "svetlana@gmail.com",
+		shortBio: "I am brave and brave, but can also be very greedy and a bit standoffish.",		
+		rejected: [9, 7],
+		accepted: [4]
 	},
 	{
-		id: "S04"
-		name: "Jane",
-		shortBio: "smokes weed",
-		likesTutorID: []
+		id: 4,
+		firstName: "Tameka",
+		lastName: "Ormond",
+		email: "tameka@gmail.com",
+		shortBio: "I am loveable and exciting, but can also be very cowardly and a bit unkind.",		
+		skills: [tutorsSkillSet[0], tutorsSkillSet[1]],
+		rejected: [],
+		accepted: []
 	},
 	{
-		id: "S05"
-		name: "Jack Sparrow",
-		shortBio: "professional hustler",
-		likesTutorID: []
-	}
-]
-
-let tutors = [
-	{
-		id: "T01"
-		name: "Debbie",
-		shortBio: "likes writing short bio's",
-		skills: tutorsSkillSet[0, 4],
-		requestsFromStudentID: ["S03", "S01"]
+		id: 5,
+		firstName: "Ayako",
+		lastName: "Firuze",
+		email: "ayako@gmail.com",
+		shortBio: "A law student with an unfortunate habit of bumping off the people around her.",		
+		rejected: [7],
+		accepted: [9, 4]
 	},
 	{
-		id: "T02"
-		name: "Nina",
-		shortBio: "has no idea what's going on",
-		skills: tutorsSkillSet[1, 3],
-		requestsFromStudentID: []
+		id: 6,
+		firstName: "Eleazar",
+		lastName: "Felix",
+		email: "eleazar@gmail.com",
+		shortBio: "A senior politician with an unfortunate habit of bumping off the people around her. I am also inspiring and smart.",		
+		rejected: [9],
+		accepted: [4]
 	},
 	{
-		id: "T03"
-		name: "Samuel",
-		shortBio: "enjoys playing the bad guy",
-		skills: tutorsSkillSet[2],
-		requestsFromStudentID: ["S01", "S04", "S05"]
-	}
+		id: 7,
+		firstName: "Sheela",
+		lastName: "Stefanija",
+		email: "sheela@gmail.com",
+		shortBio: "She enjoys extreme ironing, watching sport and duck herding.",		
+		skills: [tutorsSkillSet[4], tutorsSkillSet[3]],
+		rejected: [],
+		accepted: []
+	},
+	{
+		id: 8,
+		firstName: "Nilima",
+		lastName: "Fedelmid",
+		email: "nilima@gmail.com",
+		shortBio: "A personal trainer who enjoys watching television, working on cars and traveling.",		
+		rejected: [4, 9],
+		accepted: [7]
+	},
+	{
+		id: 9,
+		firstName: "Methodius",
+		lastName: "Balendin",
+		email: "methodius@gmail.com",
+		shortBio: "A chef at own restaurant who enjoys extreme ironing, eating out and adult colouring books",		
+		skills: [tutorsSkillSet[4], tutorsSkillSet[3]],
+		rejected: [],
+		accepted: []
+	},
+	{
+		id: 10,
+		firstName: "Kumar",
+		lastName: "Lena",
+		email: "kumar@gmail.com",
+		shortBio: "A government politician whose life is dominated by solving the murder of his brother, Leigh Lena.",		
+		rejected: [4],
+		accepted: [7, 9]
+	},
 ]
