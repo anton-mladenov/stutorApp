3622e1cf9fefa3aa0d9b0592ad399180b393122d
rbhgn <robhgn@gmail.com>
2018-06-19 11:11:11 +0200

    setup routing and created resultscontainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, Link  } from 'react-router-dom'
+import resultsContainer from './components/resultsContainer'
+import DashboardContainer from './components/DashboardContainer'
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Route exact path="/results" component={resultsContainer} />
+        <Route exact path="/dashboard" component={DashboardContainer} />
+        <Link to="/results">Go back to the results</Link>
+        <Link to="/DashboardContainer">Go back to the dashboard</Link>
+
       </div>
     );
   }
---
+++ src/components/resultsContainer.js
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react'
+
+
+export default class resultsContainer extends PureComponent {
+  render () {
+      return (
+        <div className="results-container">RESULTS CONTAINER
+        </div>
+  )
+  }
+}
