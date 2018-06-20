abb7ca76530b11545de95420f704fbcd733e5aaf
ColomboStefania <stefania.colombo3@gmail.com>
2018-06-20 09:42:40 +0200

    last from stefy

--- src/lib/matching.js
+++ src/lib/matching.js
@@ -1,115 +1,3 @@
-let tutorsSkillSet = [
-	{id: 1,
-	name: "HTML/CSS"},
-	{id: 2,
-	name: "Design"},
-	{id: 3,
-	name: "React"},
-	{id: 4,
-	name: "NodeJS"},
-	{id: 5,
-	name: "Marketing"}
-]
-let allUsers = [
-	{
-		id: 1,
-		firstName: "Hilargi",
-		lastName: "Alecto",
-		email: "hilargi@gmail.com",
-		shortBio: "I am a 47-year-old associate at a law firm who enjoys listening to music, jigsaw puzzles and baking.",
-		rejected: [4],
-		accepted: [7,9]
-	},
-	{
-		id: 2,
-		firstName: "Keara",
-		lastName: "Naseem",
-		email: "keara@gmail.com",
-		shortBio: "I am a 61-year-old CEO who enjoys donating blood, jigsaw puzzles and adult colouring books.",
-		rejected: [7, 4],
-		accepted: [9]
-	},
-	{
-		id: 3,
-		firstName: "Svetlana",
-		lastName: "Vulcan",
-		email: "svetlana@gmail.com",
-		shortBio: "I am brave and brave, but can also be very greedy and a bit standoffish.",
-		rejected: [9, 7],
-		accepted: [4]
-	},
-	{
-		id: 4,
-		firstName: "Tameka",
-		lastName: "Ormond",
-		email: "tameka@gmail.com",
-		shortBio: "I am loveable and exciting, but can also be very cowardly and a bit unkind.",
-		skills: [1],
-		rejected: [],
-		accepted: []
-	},
-	{
-		id: 5,
-		firstName: "Ayako",
-		lastName: "Firuze",
-		email: "ayako@gmail.com",
-		shortBio: "A law student with an unfortunate habit of bumping off the people around her.",
-    skills: [3],
-		rejected: [7],
-		accepted: [9, 4]
-	},
-	{
-		id: 6,
-		firstName: "Eleazar",
-		lastName: "Felix",
-		email: "eleazar@gmail.com",
-		shortBio: "A senior politician with an unfortunate habit of bumping off the people around her. I am also inspiring and smart.",
-		rejected: [9],
-		accepted: [4]
-	},
-	{
-		id: 7,
-		firstName: "Sheela",
-		lastName: "Stefanija",
-		email: "sheela@gmail.com",
-		shortBio: "She enjoys extreme ironing, watching sport and duck herding.",
-		skills: [4],
-		rejected: [],
-		accepted: [4, 6]
-	},
-	{
-		id: 8,
-		firstName: "Nilima",
-		lastName: "Fedelmid",
-		email: "nilima@gmail.com",
-		shortBio: "A personal trainer who enjoys watching television, working on cars and traveling.",
-		rejected: [9],
-		accepted: [7]
-	},
-	{
-		id: 9,
-		firstName: "Methodius",
-		lastName: "Balendin",
-		email: "methodius@gmail.com",
-		shortBio: "A chef at own restaurant who enjoys extreme ironing, eating out and adult colouring books",
-		skills: [4],
-		rejected: [],
-		accepted: [1, 10]
-	},
-	{
-		id: 10,
-		firstName: "Kumar",
-		lastName: "Lena",
-		email: "kumar@gmail.com",
-		shortBio: "A government politician whose life is dominated by solving the murder of his brother, Leigh Lena.",
-		rejected: [4],
-		accepted: [7, 9]
-	},
-]
-
-// const getTutorsWithSkillId = (skillId, allUsers) => {
-//   return getTutors(allUsers).filter(skilledUser => skilledUser.skills === skillId)
-// }
 
 const getTutors = (allUsers) => {
 	return allUsers.map(user => user)
@@ -130,11 +18,23 @@ const waitingForAcceptance = (currentUserId, allUsers) => {
                 .filter(allUsers => allUsers.accepted.includes(currentUserId))
 }
 
-const matchLogic = (id) => {
-  return waitingForAcceptance.map(user => user)
-                            .filter(currentUser.accepted.includes(user.id))
+const matchLogic = (currentUserId, allUsers) => {
+  const currentUserAccepted = allUsers.filter(a => a.id === currentUserId).reduce((acc, val) => val)
 
+  //return currentUserAccepted.accepted.map(a => waitingForAcceptance(currentUserId, allUsers).filter(b => b.id === a))
+  return currentUserAccepted.accepted.map(a => waitingForAcceptance(currentUserId, allUsers).filter(b => b.id === a)).map(c => c[0])
 }
+
+const getNamesOfTutors = () => {
+	return getTutors(allUsers).map(person => person.firstName)
+}
+
+const getNameofStudentsToBeAccepted = (currentUserId, allUsers) => {
+	return matchLogic(currentUserId, allUsers).map(person => person.firstName )
+}
+
+
+
 
 console.log(getTutors(allUsers));
 console.log(getTutorsWithSkillId(4, allUsers));
--- src/usersData/usersDummyData.js
+++ src/usersData/usersDummyData.js
@@ -106,39 +106,3 @@ let allUsers = [
 		accepted: [7, 9]
 	},
 ]
-
-// const getTutorsWithSkillId = (skillId, allUsers) => {
-//   return getTutors(allUsers).filter(skilledUser => skilledUser.skills === skillId)
-// }
-
-const getTutors = (allUsers) => {
-	return allUsers.map(user => user)
-                  .filter(userinfo => userinfo.skills)
-}
-const getTutorsWithSkillId = (skillId, allUsers) => {
-  return getTutors(allUsers)
-          .filter(skilledUser => skilledUser.skills.includes(skillId))
-}
-const getTutorsWithSkillIdAndNotAccepted = (currentUserId, skillId, allUsers) => {
-  return  getTutorsWithSkillId(skillId, allUsers)
-          .filter(user => !user.accepted.includes(currentUserId) && !user.rejected.includes(currentUserId)
-          )
-}
-//
-const waitingForAcceptance = (currentUserId, allUsers) => {
-  return allUsers.map(user => user)
-                .filter(allUsers => allUsers.accepted.includes(currentUserId))
-}
-
-const matchLogic = (currentUserId, allUsers) => {
-  const currentUserAccepted = allUsers.filter(a => a.id === currentUserId).reduce((acc, val) => val)
-
-  //return currentUserAccepted.accepted.map(a => waitingForAcceptance(currentUserId, allUsers).filter(b => b.id === a))
-  return currentUserAccepted.accepted.map(a => waitingForAcceptance(currentUserId, allUsers).filter(b => b.id === a)).map(c => c[0])
-}
-
-console.log(getTutors(allUsers));
-console.log(getTutorsWithSkillId(4, allUsers));
-console.log(getTutorsWithSkillIdAndNotAccepted(1, 4, allUsers));
-console.log(waitingForAcceptance(9, allUsers));
-console.log(matchLogic(9, allUsers));
