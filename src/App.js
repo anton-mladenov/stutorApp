494d26e7849d70f8558d2ffec48808f1d2ea30de
anton-mladenov <tonkata@mail.com>
2018-06-21 11:24:05 +0200

    working version of Home and its CSS - done. have to make it more pretty

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import ResultsContainer from "./components/ResultsContainer"
 import SelectSkillContainer from "./components/SelectSkillContainer"
 import AddSkillContainer from "./components/AddSkillContainer"
 import SearchBarContainer from "./components/SearchBarContainer"
+import Home from "./components/Home.js"
 // import ResultsContainer from './components/ResultsContainer'
 // import DashboardContainer from './components/DashboardContainer'
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+	  	<Route exact path="/home" component={Home} />
 		<Route exact path="/" component={LandingContainer} />
 		<Route exact path="/profile" component={ProfileContainer} />
 		<Route exact path="/homedash" component={HomeDashContainer} />
@@ -25,11 +26,11 @@ class App extends Component {
 		<Route exact path="/search" component={SearchBarContainer} />
         {/* <Route exact path="/dashboard" component={DashboardContainer} /> */}
 
-
+		<Link to="/home"> Home </Link>
 		<Link to="/"> Landing </Link>
 		<Link to="/profile"> Profile </Link>
 		<Link to="/homedash"> HomeDash </Link>
-    <Link to="/results">Go back to the results</Link>
+    	<Link to="/results">Go back to the results</Link>
 		<Link to="/selectskill"> Select A Skill </Link>
 		<Link to="/addskill"> Add A Skill </Link>
         {/* <Link to="/dashboard">Go back to the dashboard</Link> */}
